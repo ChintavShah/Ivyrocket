16441a3a52b4a340668ae2adb71b02c53bbc724c
Andy Liu <megamanbassx@gmail.com>
2014-06-07 21:47:37 -0400

    Homepage half done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require foundation.min
+//= require app
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
