0c89960096d5dcc1ffc5c04882cab650349b577c
Andy Liu <megamanbassx@gmail.com>
2014-06-18 17:50:23 -0400

    changed env variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,7 @@ set :deploy_to, '/home/deploy/IvyRocket'
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+
 namespace :deploy do
 
   desc 'Restart application'
